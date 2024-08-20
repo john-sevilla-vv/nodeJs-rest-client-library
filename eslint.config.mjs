dd7edf710d284edaad8d2f40ffe547b36efac83f
John Sevilla <john.sevilla@visualvault.com>
2024-08-20 13:14:09 -0400

    Add plugin for ESLint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,47 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import nodePlugin from 'eslint-plugin-n'; // https://github.com/eslint-community/eslint-plugin-n#-rules
 
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  {
+    files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}
+  },
   pluginJs.configs.recommended,
+  nodePlugin.configs["flat/recommended-script"],
+  {
+    languageOptions: { 
+      globals: {
+        ...globals.browser, 
+        ...globals.node,
+        VV : 'readonly',
+        vvClient: 'readonly',
+        $: 'readonly',
+        ControlName: 'readonly',
+        Swal: 'readonly'
+      } 
+    },
+
+    // plugins: {},
+
+    rules: {
+      'no-prototype-builtins': 'off',
+      'no-unused-vars': 'warn',
+      'no-unsafe-finally': 'warn',
+
+      // 14.18.0 as noted in nodeJs-rest-client-library (VV rest client lib)
+      "n/no-unsupported-features/es-builtins": ["error", {
+        "version": "14.18.0",
+        "ignores": []
+      }],
+      "n/no-unsupported-features/es-syntax": ["error", {
+        "version": "14.18.0",
+        "ignores": []
+      }],
+      "n/no-unsupported-features/node-builtins": ["error", {
+        "version": "14.18.0",
+        "ignores": []
+      }],
+    }
+  },
 ];
